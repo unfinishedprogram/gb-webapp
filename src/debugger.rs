b99376a4a2aff7994e1305b133ea705561053e8c
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-04 11:47:44 -0500

    Interval streams working

---
+++ src/debugger.rs
@@ -0,0 +1,34 @@
+use gameboy::{lcd::GameboyLCD, Gameboy};
+use gloo::timers::callback::Interval;
+
+#[derive(Default)]
+pub enum RunningState {
+    #[default]
+    Paused,
+    Running,
+}
+
+pub struct Debugger {
+    running_state: RunningState,
+    gameboy: Gameboy,
+}
+
+impl Debugger {
+    pub fn new() -> Self {
+        let mut gameboy = Gameboy::default();
+        gameboy.bind_lcd(GameboyLCD::default());
+        Debugger {
+            running_state: RunningState::Paused,
+            gameboy,
+        }
+    }
+
+    pub fn step(&mut self) {
+        self.gameboy.step_single_frame();
+    }
+
+    pub fn get_frame_buffer(&self) -> Vec<u8> {
+        let lcd = &self.gameboy.ppu.lcd.as_ref().unwrap();
+        return lcd.front_buffer().to_vec();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
-use sycamore::prelude::*;
+#![feature(async_closure)]
+#![feature(local_key_cell_methods)]
+
+mod screen;
+
+use std::cell::RefCell;
+
+use debugger::Debugger;
+use screen::Screen;
+mod debugger;
+use futures_util::stream::StreamExt;
+use gloo::timers::future::IntervalStream;
+use sycamore::{futures::spawn_local_scoped, prelude::*};
 
 fn main() {
+    console_error_panic_hook::set_once();
+
     sycamore::render(|cx| {
-        let a = create_signal(cx, 0);
-        let b = create_signal(cx, 0);
-        let result = create_memo(cx, || *a.get() * *b.get());
+        let buffer = create_signal::<Vec<u8>>(cx, vec![]);
+        let debugger = create_signal(cx, RefCell::new(Debugger::new()));
+        let frame_stream = IntervalStream::new(16);
+
+        // Convert a stream into a future that runs forever.
+        spawn_local_scoped(cx, async {
+            frame_stream
+                .for_each(|_| async {
+                    let debugger = debugger.get();
+                    let mut debugger = debugger.borrow_mut();
+                    debugger.step();
+                    buffer.set(debugger.get_frame_buffer())
+                })
+                .await
+        });
 
         view! { cx,
-            button(
-                on:click= |_| a.set(*a.get() + 1)
-            ) {(a.get())}
-
-            span { "*" }
-
-            button(
-                on:click= |_| b.set(*b.get() + 1)
-            ) {(b.get())}
-
-            span { "=" }
-            span { (result.get()) }
+            div {
+                Screen(buffer = buffer)
+            }
         }
     });
 }
